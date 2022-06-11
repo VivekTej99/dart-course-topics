415d4967aee63882981bf3ba5209db0d638a66aa
Vivek <vivektej2299@gmail.com>
2022-06-11 20:01:24 +0530

    Add controlflow topic

---
+++ bin/control_flow.dart
@@ -0,0 +1,23 @@
+void main(List<String> args) {
+  String color = 'hello';
+
+  if (color == 'red') {
+    print(true);
+  } else if (color == 'green') {
+    print("Green");
+  } else {
+    print("BLUE");
+  }
+
+  if (true) print("Hello User!");
+
+  if (color.isNotEmpty) {
+    print("color is not empty var");
+  }
+
+  String? nullValue;
+
+  if (nullValue == null) {
+    print("The value is null");
+  }
+}
